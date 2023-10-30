341326a8deaaa605fe635fa5a16de4f4516fca9c
pietro <pietroporfirio22@gmail.com>
2023-10-30 14:42:48 -0300

    adicionando um console log no arquivo app.js

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
+
+console.log("o paulo fex esse commit sem vc ver na aula")
+console.log("enviando para o github")
